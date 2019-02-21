f6ac0a658d314e66088a7e26308a0acf2b415d87
Andres Saldaña Aguilar <pollyketpo@gmail.com>
2019-02-21 17:08:22 -0600

    agregar y quitar agentes de Json

--- practica1/main.py
+++ practica1/main.py
@@ -1,6 +1,6 @@
 from AgentManager import AgentManager
 
-mm = AgentManager()
+am = AgentManager()
 
         
 def main():
@@ -10,6 +10,7 @@ def main():
 def menu():
     while 1:
         #mostrar por cada agente: status (up o down), interfaces y su estatus 
+        am.readJson()
         print("1. Agregar Agente")
         print("2. Eliminar Agente")
         print("3. Estado de un agente")
@@ -17,35 +18,36 @@ def menu():
         print("5. Salir")
         opc = int(input())
         if opc == 1:
-            idAgente = input("IdAgente: ")
+            agentID = input("agentID: ")
             hostname = input("Hostname: ")
             version = input("Version: ")
             port = int(input("Port: "))
             comunity = input("Comunity: ")
 
-            if mm.addAgent(
-                idAgente, hostname, version, port, comunity
+            if am.addAgent(
+                agentID, hostname, version, port, comunity
             ):
-                print(idAgente + " registrado.")
+                print(agentID + " registrado.")
             else:
-                print("Ya existe el idAgente.")
+                print("Ya existe el agentID.")
 
         if opc == 2:
-            idAgente = input("IdAgente: ")
-            if mm.removeAgent(idAgente):
-                print(idAgente + " eliminado.")
+            agentID = input("agentID: ")
+            if am.removeAgent(agentID):
+                print(agentID + " eliminado.")
             else:
-                print("No se encontro el idAgente.")
+                print("No se encontro el agentID.")
 
         if opc == 3:
-            #idAgente = input("IdAgente: ")
-            mm.readJson()
+            agentID = input("agentID: ")
+            agent = am.getAgent(agentID)
+            print(agent)
             
         if opc == 4:
-            continue;
+            continue
 
         if opc == 5:
             print("Cerrando monitores.")
-            break;
+            break
 
 main()
