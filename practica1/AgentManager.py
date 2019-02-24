2db0fcbe2db1a597c73f609db8f1ed79e8184934
andresSaldanaAguilar <andres.saldana.aguilar@gmail.com>
2019-02-24 11:03:44 -0600

    identation

--- practica1/AgentManager.py
+++ practica1/AgentManager.py
@@ -96,8 +96,8 @@ class AgentManager():
     def agentMonitoring(
 	    self
 	):
-        for item in data:
-            print(data['hostname'])
+		for k,v in self.data.items()
+			print(k+"_"+v)
 
 
 
