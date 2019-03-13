0ab3fb8eef515397f912291191f4272c353685e8
Andres Saldaña Aguilar <pollyketpo@gmail.com>
2019-03-12 23:06:56 -0600

    test

--- practica2/trendGraph.py
+++ practica2/trendGraph.py
@@ -44,67 +44,64 @@ class trendGraph(threading.Thread):
 								"CDEF:pmul=pin,pout,*",
 								"CDEF:pused=pmul,100,*", 
 								"CDEF:carga=pused,tmul,/",    
+                    
+					 "AREA:carga#00FF00:Carga CPU",
+                     "LINE1:30",
+                     "AREA:5#ff000022:stack",
+                     
+                     #datos del CPU
+                     "VDEF:CPUlast=carga,LAST",
+                     "VDEF:CPUmin=carga,MINIMUM",
+                     "VDEF:CPUavg=carga,AVERAGE",
+                     "VDEF:CPUmax=carga,MAXIMUM",
+                     "VDEF:cargaSTDEV=carga,STDEV",
+                     "VDEF:cargaLAST=carga,LAST",
+                     
 
+                    #grafica de los datos del CPU
+                    "COMMENT:Now          Min             Avg             Max",
+                     "GPRINT:CPUlast:%12.0lf%s",
+                     "GPRINT:CPUmin:%10.0lf%s",
+                     "GPRINT:CPUavg:%13.0lf%s",
+                     "GPRINT:CPUmax:%13.0lf%s",
+                     "GPRINT:cargaSTDEV:%6.2lf %SSTDEV",
+                     "GPRINT:cargaLAST:%6.2lf %SLAST",
+                    "PRINT:CPUmax:%6.2lf %S",
 
-								
-								"AREA:carga#00FF00:Carga CPU",
-								"LINE1:30",
-								"AREA:5#ff000022:stack",
-								
-								#datos del CPU
-								#"VDEF:CPUlast=carga,LAST",
-								#"VDEF:CPUmin=carga,MINIMUM",
-								#"VDEF:CPUavg=carga,AVERAGE",
-								#"VDEF:CPUmax=carga,MAXIMUM",
-								#"VDEF:cargaSTDEV=carga,STDEV",
-								#"VDEF:cargaLAST=carga,LAST",
-								
+                    #método de mínimos cuadrados
+                     "VDEF:m=carga,LSLSLOPE",
+                     "VDEF:b=carga,LSLINT",
+                     'CDEF:tendencia=carga,POP,m,COUNT,*,b,+',
+                     "LINE2:tendencia#FFBB00", 
 
-								#grafica de los datos del CPU
-								#"COMMENT:Now          Min             Avg             Max",
-								# "GPRINT:CPUlast:%12.0lf%s",
-								# "GPRINT:CPUmin:%10.0lf%s",
-								# "GPRINT:CPUavg:%13.0lf%s",
-								# "GPRINT:CPUmax:%13.0lf%s",
-								# "GPRINT:cargaSTDEV:%6.2lf %SSTDEV",
-								# "GPRINT:cargaLAST:%6.2lf %SLAST",
-								#"PRINT:CPUmax:%6.2lf %S",
+                     #establecer el umbral1
+                    "CDEF:umbral25=carga,"+str(self.umbral1)+",LT,0,carga,IF",
+                    "AREA:carga#00FF00:Carga del CPU",
+                     "AREA:umbral25#FF9F00:Tráfico de carga mayor que "+str(self.umbral1)+"",
+                     "HRULE:"+str(self.umbral1)+"#0EBC28:Umbral 1 - "+str(self.umbral1)+"%",
 
-								#m�todo de m�nimos cuadrados
-								"VDEF:m=carga,LSLSLOPE",
-								"VDEF:b=carga,LSLINT",
-								'CDEF:tendencia=carga,POP,m,COUNT,*,b,+',
-								"LINE2:tendencia#FFBB00", 
+                    #establecer el umbral 2
+                     "CDEF:umbral30=carga,"+str(self.umbral2)+",LT,0,carga,IF",
+                    "AREA:carga#00FF00:Carga del CPU",
+                     "AREA:umbral30#FF9F00:Tráfico de carga mayor que "+str(self.umbral2)+"",
+                     "HRULE:"+str(self.umbral2)+"#E1D71E:Umbral 2 - "+str(self.umbral2)+"%",
 
-								#establecer el umbral1
-								"CDEF:umbral25=carga,"+str(self.umbral1)+",LT,0,carga,IF",
-								"AREA:carga#00FF00:Carga del CPU",
-								"AREA:umbral25#FF9F00:Tr�fico de carga mayor que "+str(self.umbral1)+"",
-								"HRULE:"+str(self.umbral1)+"#0EBC28:Umbral 1 - "+str(self.umbral1)+"%",
+                    #establecer el umbral 3
+                     "CDEF:umbral35=carga,"+str(self.umbral3)+",LT,0,carga,IF",
+                    "AREA:carga#00FF00:Carga del CPU",
+                     "AREA:umbral35#FF9F00:Tráfico de carga mayor que "+str(self.umbral3)+"",
+                     "HRULE:"+str(self.umbral3)+"#FF0000:Umbral 3 - "+str(self.umbral3)+"%",
+                     
+                     #detectar punto de corte
+                    'CDEF:cintersect=tendencia,0,EQ,tendencia,0,IF,'+str(self.umbral1)+',+,m,/,b,+,100,/,900,*,'+str(self.initTime)+',+',
+                    "VDEF:pintersect=cintersect,MAXIMUM",
+                    "COMMENT: Punto",
+                    "GPRINT:pintersect:%8.0lf",
+                    "PRINT:pintersect:%8.0lf %S",
 
-								#establecer el umbral 2
-								"CDEF:umbral30=carga,"+str(self.umbral2)+",LT,0,carga,IF",
-								"AREA:carga#00FF00:Carga del CPU",
-								"AREA:umbral30#FF9F00:Tr�fico de carga mayor que "+str(self.umbral2)+"",
-								"HRULE:"+str(self.umbral2)+"#E1D71E:Umbral 2 - "+str(self.umbral2)+"%",
-
-								#establecer el umbral 3
-								"CDEF:umbral35=carga,"+str(self.umbral3)+",LT,0,carga,IF",
-								"AREA:carga#00FF00:Carga del CPU",
-								"AREA:umbral35#FF9F00:Tr�fico de carga mayor que "+str(self.umbral3)+"",
-								"HRULE:"+str(self.umbral3)+"#FF0000:Umbral 3 - "+str(self.umbral3)+"%",
-								
-								
-
-								#detectar punto de corte
-								'CDEF:cintersect=tendencia,0,EQ,tendencia,0,IF,'+str(self.umbral1)+',+,m,/,b,+,100,/,3600,*,'+str(self.initTime)+',+',
-								"VDEF:pintersect=cintersect,MAXIMUM",
-								"COMMENT: Punto",
-								"GPRINT:pintersect:%8.0lf",
-								"PRINT:pintersect:%6.2lf %S",
-
-								#detectar cuando se sale del umbral
-			)				              
+                     #detectar cuando se sale del umbral
+                     "PRINT:pintersect: %c:strftime",
+        )             
 									
 						
 				#aqui manejamos el caso de los cores, pero este valor ya viene en porcentaje, asi que no hay que calcular nada			               
@@ -116,74 +113,75 @@ class trendGraph(threading.Thread):
 								"--vertical-label="+self.unit,
 								"DEF:carga="+self.filename+".rrd:in:AVERAGE",                  
 
-								
-								"AREA:carga#00FF00:Carga CPU",
-								"LINE1:30",
-								"AREA:5#ff000022:stack",
-								
-								#datos del CPU
-								#"VDEF:CPUlast=carga,LAST",
-								#"VDEF:CPUmin=carga,MINIMUM",
-								#"VDEF:CPUavg=carga,AVERAGE",
-								#"VDEF:CPUmax=carga,MAXIMUM",
-								#"VDEF:cargaSTDEV=carga,STDEV",
-								#"VDEF:cargaLAST=carga,LAST",
-								
+                     "AREA:carga#00FF00:Carga CPU",
+                     "LINE1:30",
+                     "AREA:5#ff000022:stack",
+                     
+                     #datos del CPU
+                     "VDEF:CPUlast=carga,LAST",
+                     "VDEF:CPUmin=carga,MINIMUM",
+                     "VDEF:CPUavg=carga,AVERAGE",
+                     "VDEF:CPUmax=carga,MAXIMUM",
+                     "VDEF:cargaSTDEV=carga,STDEV",
+                     "VDEF:cargaLAST=carga,LAST",
+                     
 
-								#grafica de los datos del CPU
-								#"COMMENT:Now          Min             Avg             Max",
-								# "GPRINT:CPUlast:%12.0lf%s",
-								# "GPRINT:CPUmin:%10.0lf%s",
-								# "GPRINT:CPUavg:%13.0lf%s",
-								# "GPRINT:CPUmax:%13.0lf%s",
-								# "GPRINT:cargaSTDEV:%6.2lf %SSTDEV",
-								# "GPRINT:cargaLAST:%6.2lf %SLAST",
-								#"PRINT:CPUmax:%6.2lf %S",
+                    #grafica de los datos del CPU
+                    "COMMENT:Now          Min             Avg             Max",
+                     "GPRINT:CPUlast:%12.0lf%s",
+                     "GPRINT:CPUmin:%10.0lf%s",
+                     "GPRINT:CPUavg:%13.0lf%s",
+                     "GPRINT:CPUmax:%13.0lf%s",
+                     "GPRINT:cargaSTDEV:%6.2lf %SSTDEV",
+                     "GPRINT:cargaLAST:%6.2lf %SLAST",
+                    "PRINT:CPUmax:%6.2lf %S",
 
-								#m�todo de m�nimos cuadrados
-								"VDEF:m=carga,LSLSLOPE",
-								"VDEF:b=carga,LSLINT",
-								'CDEF:tendencia=carga,POP,m,COUNT,*,b,+',
-								"LINE2:tendencia#FFBB00", 
+                    #método de mínimos cuadrados
+                     "VDEF:m=carga,LSLSLOPE",
+                     "VDEF:b=carga,LSLINT",
+                     'CDEF:tendencia=carga,POP,m,COUNT,*,b,+',
+                     "LINE2:tendencia#FFBB00", 
 
-								#establecer el umbral1
-								"CDEF:umbral25=carga,"+str(self.umbral1)+",LT,0,carga,IF",
-								"AREA:carga#00FF00:Carga del CPU",
-								"AREA:umbral25#FF9F00:Tr�fico de carga mayor que "+str(self.umbral1)+"",
-								"HRULE:"+str(self.umbral1)+"#0EBC28:Umbral 1 - "+str(self.umbral1)+"%",
+                     #establecer el umbral1
+                    "CDEF:umbral25=carga,"+str(self.umbral1)+",LT,0,carga,IF",
+                    "AREA:carga#00FF00:Carga del CPU",
+                     "AREA:umbral25#FF9F00:Tráfico de carga mayor que "+str(self.umbral1)+"",
+                     "HRULE:"+str(self.umbral1)+"#0EBC28:Umbral 1 - "+str(self.umbral1)+"%",
 
-								#establecer el umbral 2
-								"CDEF:umbral30=carga,"+str(self.umbral2)+",LT,0,carga,IF",
-								"AREA:carga#00FF00:Carga del CPU",
-								"AREA:umbral30#FF9F00:Tr�fico de carga mayor que "+str(self.umbral2)+"",
-								"HRULE:"+str(self.umbral2)+"#E1D71E:Umbral 2 - "+str(self.umbral2)+"%",
+                    #establecer el umbral 2
+                     "CDEF:umbral30=carga,"+str(self.umbral2)+",LT,0,carga,IF",
+                    "AREA:carga#00FF00:Carga del CPU",
+                     "AREA:umbral30#FF9F00:Tráfico de carga mayor que "+str(self.umbral2)+"",
+                     "HRULE:"+str(self.umbral2)+"#E1D71E:Umbral 2 - "+str(self.umbral2)+"%",
 
-								#establecer el umbral 3
-								"CDEF:umbral35=carga,"+str(self.umbral3)+",LT,0,carga,IF",
-								"AREA:carga#00FF00:Carga del CPU",
-								"AREA:umbral35#FF9F00:Tr�fico de carga mayor que "+str(self.umbral3)+"",
-								"HRULE:"+str(self.umbral3)+"#FF0000:Umbral 3 - "+str(self.umbral3)+"%",
-								
-								
+                    #establecer el umbral 3
+                     "CDEF:umbral35=carga,"+str(self.umbral3)+",LT,0,carga,IF",
+                    "AREA:carga#00FF00:Carga del CPU",
+                     "AREA:umbral35#FF9F00:Tráfico de carga mayor que "+str(self.umbral3)+"",
+                     "HRULE:"+str(self.umbral3)+"#FF0000:Umbral 3 - "+str(self.umbral3)+"%",
+                     
+                     
 
-								#detectar punto de corte
-								'CDEF:cintersect=tendencia,0,EQ,tendencia,0,IF,'+str(self.umbral1)+',+,m,/,b,+,100,/,3600,*,'+str(self.initTime)+',+',
-								"VDEF:pintersect=cintersect,MAXIMUM",
-								"COMMENT: Punto",
-								"GPRINT:pintersect:%8.0lf",
-								"PRINT:pintersect:%6.2lf %S",
+                     #detectar punto de corte
+                    'CDEF:cintersect=tendencia,0,EQ,tendencia,0,IF,'+str(self.umbral1)+',+,m,/,b,+,100,/,900,*,'+str(self.initTime)+',+',
+                    "VDEF:pintersect=cintersect,MAXIMUM",
+                    "COMMENT: Punto",
+                    "GPRINT:pintersect:%8.0lf",
+                    "PRINT:pintersect:%8.0lf %S",
 
-								#detectar cuando se sale del umbral
-			)
+                     #detectar cuando se sale del umbral
+                     "PRINT:pintersect: %c:strftime",
+        )
 			
-			lastValue = ret['print[0]']
-			fechaPredict = ret['print[1]']
-			if checkErrors(lastValue,self.umbral1):
-				print("manda mail")
-				sendEmail(self.filename)
+		lastValue = ret['print[0]']
+		fechaPredict = ret['print[1]']
+		fechaBien = ret['print[2]']
+		print("Fecha bien"+fechaBien)
+		if checkErrors(lastValue,self.umbral1):
+			print("manda mail")
+			#sendEmail(self.filename)
 
-			print("ultimo valor max: "+lastValue)
-			print("fecha en la cual llega al tope: "+fechaPredict)
+		print("ultimo valor max: "+lastValue)
 
 
-			time.sleep(5)
+		time.sleep(5)
