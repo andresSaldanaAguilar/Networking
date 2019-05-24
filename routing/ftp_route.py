38b38e62ed9342fdb9d9976dc979512a5d247b46
andresSaldanaAguilar <andres.saldana.aguilar@gmail.com>
2019-05-23 22:58:03 -0500

    script para conseguir el archivo de config

--- routing/ftp_route.py
+++ routing/ftp_route.py
@@ -1,5 +1,14 @@
+import os
 from ftplib import FTP
 
 #domain name or server ip:
-ftp = FTP('10.22.0.2')
-print(ftp.login(user='rcp', passwd = 'rcp'))
+print("Enter router IP to extract config:")
+ip = str(input())
+ftp = FTP(ip)
+ftp.login(user='rcp', passwd = 'rcp')
+
+local_filename = os.path.join(r'/home/andres/Desktop', 'conf_'+ip)
+handle = open(local_filename, 'wb')
+ftp.retrbinary('RETR %s' % 'startup-config', handle.write)
+
+
