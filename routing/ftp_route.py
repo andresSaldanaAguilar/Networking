de4dc72481f981b5063c04eacd4563a3735b1027
GabSaldana <ela.ri.bag@outlook.com>
2019-05-24 23:22:20 -0500

    trabajando en enviar archivos

--- routing/ftp_route.py
+++ routing/ftp_route.py
@@ -2,13 +2,24 @@ import os
 from ftplib import FTP
 
 #domain name or server ip:
-print("Enter router IP to extract config:")
-ip = str(input())
+print("Enter router IP address:")
+ip = input()
 ftp = FTP(ip)
 ftp.login(user='rcp', passwd = 'rcp')
 
-local_filename = os.path.join(r'/home/andres/Desktop', 'conf_'+ip)
-handle = open(local_filename, 'wb')
-ftp.retrbinary('RETR %s' % 'startup-config', handle.write)
+print("Get or Put? (g/p)")
+option = input().lower()
 
+if option == "g":
+    local_filename = os.path.join(r''+os.path.dirname(os.path.realpath(__file__)), 'conf_'+ip)
+    file = open(local_filename, 'wb')
+    print(ftp.retrbinary('RETR %s' % 'startup-config', file.write))
+    file.close()
+else:
+    print("Filepath to send:")
+    path = input()
+    file = open(path,'rb')                  # file to send
+    print(ftp.storbinary('startup-config', file))     # send the file
+    file.close()                                    # close file and FTP
 
+ftp.quit()
